cac40545c6b78fed23a417a67ed93d20c1c9f216
Rob Schaller <rmschaller@gmail.com>
2025-01-19 15:40:32 -0600

    added more defines for timer values

--- src/timerValues.h
+++ src/timerValues.h
@@ -2,3 +2,7 @@
 #define OLED_DEFAULT_TIME 15                                // Default time to turn on display is seconds
 #define WIFI_RESET_TIME 5                                   // Reset WiFi Parameters after xx seconds
 #define EEPROM_RESET_TIME 15                                // Reset EEPROM after xx seconds (EEPROM_RESET_TIME ***MUST*** BE > WIFI_RESET_TIME; No checks made)
+// MQTT 
+#define HA_MQTT_RETRY_TIME 30000                            // HA MQTT server retry counter (30 seconds)
+#define MQTT_RETRY_TIME 30000                               // MQTT server retry counter (30 seconds)
+#define WEB_TIMEOUT 2000                                    // Web client wait time
